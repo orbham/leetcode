335bdc9178fdcc5b36dbadb9bc617c82f3c843e7
Hayden Truax <158366929+orbham@users.noreply.github.com>
2025-01-13 22:17:33 -0800

    Time: 0 ms (100%), Space: 50.9 MB (49.48%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+ /*
+    var difference = ??
+    subtract target from number
+    return difference location, i if they add up to target
+
+    for (let i = 0; i < nums.length, i++){
+        var difference = None
+
+    }
+ 
+ */
+
+var twoSum = function(nums, target) {
+    const hashmap = {};
+    for (let i = 0; i < nums.length; i++){ 
+        var difference = target - nums[i] 
+        if (!(difference in hashmap)){ 
+            hashmap[nums[i]] = i
+        } else { // 
+            return [hashmap[difference], i] 
+        }
+    } 
+};
